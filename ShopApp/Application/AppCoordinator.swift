4273e33b3e13be3875358973494ab5a105c05dca
Alexander6Shevtsov <axrsvr@gmail.com>
2025-08-18 12:57:11 +0300

    add AppCoordinator

--- ShopApp/Application/AppCoordinator.swift
+++ ShopApp/Application/AppCoordinator.swift
@@ -5,4 +5,32 @@
 //  Created by Alexander Shevtsov on 18.08.2025.
 //
 
-import Foundation
+import UIKit
+
+final class AppCoordinator {
+    private let window: UIWindow
+    private let navigationController = UINavigationController()
+    
+    init(window: UIWindow) {
+        self.window = window
+        navigationController.navigationBar.prefersLargeTitles = true
+    }
+    
+    func start() {
+        let vc = PlaceholderViewController()
+        navigationController.setViewControllers([vc], animated: false)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+    
+//    func showRegistration() { }
+//    func showMain() { }
+}
+
+final class PlaceholderViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "ShopApp"
+    }
+}
