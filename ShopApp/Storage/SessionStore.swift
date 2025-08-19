5e0f732434409fab0bbcb0fc414ec92dcaea9fbd
Alexander6Shevtsov <axrsvr@gmail.com>
2025-08-19 11:27:06 +0300

    add UserDefaultsSessionStore

--- ShopApp/Storage/SessionStore.swift
+++ ShopApp/Storage/SessionStore.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+protocol SessionStoreType {
+    var userName: String? { get set }
+    var isRegistered: Bool { get set }
+}
+
+final class UserDefaultsSessionStore: SessionStoreType {
+    private enum Keys {
+        static let userName = "session.username"
+        static let isRegistered = "session.isRegistered"
+    }
+    
+    private let defaults: UserDefaults
+    
+    init(defaults: UserDefaults = .standard) {
+        self.defaults = defaults
+    }
+    
+    var userName: String? {
+        get { defaults.string(forKey: Keys.userName) }
+        set { defaults.set(newValue, forKey: Keys.userName) }
+    }
+    
+    var isRegistered: Bool {
+        get { defaults.bool(forKey: Keys.isRegistered) }
+        set { defaults.set(newValue, forKey: Keys.isRegistered) }
+    }
+}
