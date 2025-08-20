eacae8c4c1603f1e4914491be68b2d33156583bb
Alexander6Shevtsov <axrsvr@gmail.com>
2025-08-20 20:35:56 +0300

    add Cell

--- ShopApp/Main/MVP/Cell.swift
+++ ShopApp/Main/MVP/Cell.swift
@@ -5,4 +5,39 @@
 //  Created by Alexander Shevtsov on 20.08.2025.
 //
 
-import Foundation
+import UIKit
+
+final class Cell: UITableViewCell {
+    static let reuseID = "Cell"
+    
+    private let titleLabel = UILabel()
+    private let priceLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        titleLabel.numberOfLines = 2
+        priceLabel.font = .preferredFont(forTextStyle: .subheadline)
+        priceLabel.textColor = .secondaryLabel
+        
+        let stack = UIStackView(arrangedSubviews: [titleLabel, priceLabel])
+        stack.axis = .vertical
+        stack.spacing = 4
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(stack)
+        NSLayoutConstraint.activate([
+            stack.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
+            stack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            stack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            stack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12)
+        ])
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    func configure(with vm: CellViewModel) {
+        titleLabel.text = vm.title
+        priceLabel.text = vm.price
+    }
+}
