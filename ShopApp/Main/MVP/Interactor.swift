b9ac364f53710af96476948e7f7aaea71f5e3f1c
Alexander6Shevtsov <axrsvr@gmail.com>
2025-08-20 20:05:24 +0300

    add Interactor

--- ShopApp/Main/MVP/Interactor.swift
+++ ShopApp/Main/MVP/Interactor.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+final class Interactor: InteractorType {
+    private let api: ProductsAPIType
+    
+    init(api: ProductsAPIType) {
+        self.api = api
+    }
+    
+    func fetchProducts() async throws -> [Product] {
+        try await api.fetchProducts()
+    }
+}
